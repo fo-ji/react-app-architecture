50a018cb2eed7899e1432b06efd9b41827a45467
fo-ji <fumiya1753@gmail.com>
2023-05-18 17:08:44 +0900

    add storybook

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,27 @@
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { Button, type ButtonProps } from './button';
+
+const meta: Meta = {
+  title: 'Components/Button',
+  component: Button,
+};
+export default meta;
+
+const Template: StoryFn<ButtonProps> = (props) => (
+  <Button {...props} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: 'Click Me',
+};
+
+export const WithIcon = Template.bind({});
+
+WithIcon.args = {
+  children: 'Click Me',
+  icon: <PlusSquareIcon />,
+};
---
+++ src/components/form/input-field.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryFn } from '@storybook/react';
+
+import {
+  InputField,
+  type InputFieldProps,
+} from './input-field';
+
+const meta: Meta = {
+  title: 'Components/Form/InputField',
+  component: InputField,
+};
+
+export default meta;
+
+const Template: StoryFn<InputFieldProps> = (props) => (
+  <InputField {...props} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: 'Name',
+};
+
+export const WithError = Template.bind({});
+
+WithError.args = {
+  label: 'Name',
+  error: {
+    type: 'required',
+    message: 'Name is required!',
+  },
+};
