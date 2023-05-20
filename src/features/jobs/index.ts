b981ea19b0a0c17cca6afc5a0d35a5eee79527ce
fo-ji <fumiya1753@gmail.com>
2023-05-20 19:57:31 +0900

    implement public-job-page in ssr

--- src/features/jobs/index.ts
+++ src/features/jobs/index.ts
@@ -2,7 +2,7 @@
 // export * from './components/create-job-form';
 // export * from './components/dashboard-job-info';
 export * from './components/jobs-list';
-// export * from './components/public-job-info';
+export * from './components/public-job-info';
 
 // types:
 export * from './types';
